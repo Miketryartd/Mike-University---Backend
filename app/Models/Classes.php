a3f5500abb6b73a7c97722ed5a7a4ca406f46076
Miketryartd <mikeleano26@gmail.com>
2026-07-22 18:44:21 +0800

    updated Classes model to include fk user email and removed class name renamed to subject

--- app/Models/Classes.php
+++ app/Models/Classes.php
@@ -10,7 +10,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 
-#[Fillable(["class_name", "creator_id", "class_code"])]
+#[Fillable(["subject_name", "creator_id", "class_code", "user_email", "class_section"])]
 
 class Classes extends Model
 {
@@ -21,6 +21,9 @@ class Classes extends Model
         return $this->belongsTo(User::class, "creator_id");
     }
 
+    public function email(): BelongsTo{
+        return $this->belongsTo(User::class, "user_email");
+    }
     public function students(): HasMany{
         return $this->hasMany(User::class);
     }
