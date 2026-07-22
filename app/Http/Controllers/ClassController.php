f050a24279de14b296608bfa5b0c89a3937147d3
Miketryartd <mikeleano26@gmail.com>
2026-07-22 18:43:49 +0800

    updated class controller for creation and added fetch class func

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -13,7 +13,8 @@ public function CreateClass(Request $request){
     
  $user = Auth::user();
       $validated = $request->validate([
-        "class_name" => "string|required|max:255",
+        "subject_name" => "string|required|max:255",
+        "class_section" => "string|required|max:255",
         "class_code" => "nullable|string"
    
       ]);
@@ -23,9 +24,11 @@ public function CreateClass(Request $request){
         return response()->json(["message" => "User not logged in"], 422);
     }
         $newClass = Classes::create([
-           "class_name" => $validated["class_name"],
+           "subject_name" => $validated["subject_name"],
+           "class_section" => $validated["class_section"],
            "class_code" => $validated["class_code"],
            "creator_id" => $user->id ,
+           "user_email" => $user->email
            
         ]);
         return response()->json(["message" => "Class created succesfully", "class" => $newClass], 200);
@@ -35,4 +38,21 @@ public function CreateClass(Request $request){
    }
 }
     
+public function GetClasses(){
+
+  try{
+   $user = Auth::user();
+   if (!$user || Auth::check() === false){
+      return response()->json(["message" => "User not auth"], 422);
+   }
+   $fetchClasses = Classes::query()->where("id", "LIKE", "%{$user->id}%")->orWhere("user_email", 'LIKE', "%{$user->email}%")->paginate(10);
+   if (!$fetchClasses){
+      return response()->json(["message" => "No classes found"], 404);
+   } else {
+      return response()->json(["message" => "Succesfully fetched classes", "classes" => $fetchClasses], 200);
+  }
+   } catch (\Exception $e){
+      return response()->json(["message" => "Error getting user classes", "error" => $e->getMessage()], 500);
+   }
+}
 }
